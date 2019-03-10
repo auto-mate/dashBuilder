1c13ba0b3f8ac95c20d4e10188a6c84dd7d90bcc
auto-mate <aandaharper@gmail.com>
2019-03-10 22:48:46 +0000

    Save And Load

--- LoadFileToEditor.php
+++ LoadFileToEditor.php
@@ -1,11 +1,11 @@
 <?php 
 /* 'firstTest.html' to be replaced with variable */
-$fileContents = file_get_contents( 'userfiles/' . 'firstTest.html' );
+$fileContents = file_get_contents( './userfiles/' .  $_POST['PostFileName'] );
 
 preg_match( "/<\/head>\\s*<body>/", $fileContents, $x, PREG_OFFSET_CAPTURE );
 
 
-$out = substr( $fileContents, $x[0][1], strlen( $fileContents ) );
+$out = substr( $fileContents, $x[0][1], strlen( $fileContents ) ) ;
 $out = str_replace( "</head>", "", $out );
 $out = str_replace( "</html>", "", $out );
 $out = str_replace( "<body>",  "", $out );
